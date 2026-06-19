d4061043443d91d6270f946a8d3b902ff5fe1841
Phan Van Nam <phannam1405@gmail.com>
2026-06-19 09:30:48 +0700

    Implement MULTIPLY operation in calculate function

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,17 +1,16 @@
 package calculator;
-
 public class Calculator {
-
     public enum SIGN {
         PLUS, MINUS, MULTIPLY, DIVIDE
     }
-
     private Double calculate(SIGN sign, Double arg1, Double arg2) {
         switch (sign) {
             case PLUS:
                 return arg1 + arg2;
             case MINUS:
                 return arg1 - arg2;
+            case MULTIPLY:
+                return arg1 * arg2;
             default:
                 break;
         }

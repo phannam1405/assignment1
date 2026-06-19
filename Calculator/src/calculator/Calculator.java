c9477096cc223011090b833d9d7e6cb89f442e96
Phan Van Nam <phannam1405@gmail.com>
2026-06-19 09:10:39 +0700

    Add Calculator base with calculate function skeleton

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Calculator {
+
+    public enum SIGN {
+        PLUS, MINUS, MULTIPLY, DIVIDE
+    }
+
+    private Double calculate(SIGN sign, Double arg1, Double arg2) {
+        switch (sign) {
+            default:
+                break;
+        }
+        return 0d;
+    }
+}
